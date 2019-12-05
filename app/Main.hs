5e0242c6d4cc8e48d7e3bd0616562e92fb960d51
Nima Boscarino <nima.boscarino@gmail.com>
2019-12-04 21:58:08 -0800

    q 2, refafctor...

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,12 @@
 module Main where
 
 -- import Lib
-import Question3
+import Question2
 
 import System.IO     
 
 main :: IO ()
-main = withFile "./inputs/3-a.txt" ReadMode (\handle -> do  
+main = withFile "./inputs/2-a.txt" ReadMode (\handle -> do  
     contents <- hGetContents handle
-    let val = question3b contents
+    let val = question2a contents
     print val)
